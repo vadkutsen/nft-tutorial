a5307840a8980e86dd6531ffba2abf4a47786614
BenKurrek <bkurrek@uwaterloo.ca>
2021-11-08 13:42:34 -0500

    Initial implementation of minting

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -7,12 +7,14 @@ use near_sdk::{
     env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, StorageUsage,
 };
 
+use crate::internal::*;
 pub use crate::metadata::*;
 pub use crate::mint::*;
 pub use crate::nft_core::*;
 pub use crate::approval::*;
 pub use crate::royalty::*;
 
+mod internal;
 mod approval; 
 mod enumeration; 
 mod metadata; 
@@ -29,9 +31,23 @@ near_sdk::setup_alloc!();
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    /*
-        FILL THIS IN
-    */
+    //contract owner
+    pub owner_id: AccountId,
+
+    //keeps track of all the token IDs for a given account
+    pub tokens_per_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
+
+    //keeps track of the token struct for a given token ID
+    pub tokens_by_id: LookupMap<TokenId, Token>,
+
+    //keeps track of the token metadata for a given token ID
+    pub token_metadata_by_id: UnorderedMap<TokenId, TokenMetadata>,
+
+    /// The storage size in bytes for one account.
+    pub extra_storage_in_bytes_per_token: StorageUsage,
+    
+    //keeps track of the metadata for the contract
+    pub metadata: LazyOption<NFTMetadata>,
 }
 
 /// Helper structure to for keys of the persistent collections.
@@ -51,21 +67,57 @@ pub enum StorageKey {
 impl Contract {
     #[init]
     pub fn new_default_meta(owner_id: ValidAccountId) -> Self {
-        /*
-            FILL THIS IN
-        */
+        Self::new(
+            owner_id,
+            NFTMetadata {
+                spec: "nft-1.0.0".to_string(),
+                name: "NFT Tutorial Contract".to_string(),
+                symbol: "GOTEAM".to_string(),
+                icon: None,
+                base_uri: None,
+                reference: None,
+                reference_hash: None,
+            },
+        )
     }
 
     #[init]
-    pub fn new(owner_id: ValidAccountId, metadata: NFTMetadata) {
-        /*
-            FILL THIS IN
-        */
+    pub fn new(owner_id: ValidAccountId, metadata: NFTMetadata) -> Self {
+        let this = Self {
+            tokens_per_owner: LookupMap::new(StorageKey::TokensPerOwner.try_to_vec().unwrap()),
+            tokens_by_id: LookupMap::new(StorageKey::TokensById.try_to_vec().unwrap()),
+            token_metadata_by_id: UnorderedMap::new(
+                StorageKey::TokenMetadataById.try_to_vec().unwrap(),
+            ),
+            owner_id: owner_id.into(),
+            extra_storage_in_bytes_per_token: 0,
+            metadata: LazyOption::new(
+                StorageKey::NftMetadata.try_to_vec().unwrap(),
+                Some(&metadata),
+            ),
+        };
+
+        this
     }
 
     fn measure_min_token_storage_cost(&mut self) {
-        /*
-            FILL THIS IN
-        */
+        let initial_storage_usage = env::storage_usage();
+        let tmp_account_id = "a".repeat(64);
+        let u = UnorderedSet::new(
+            StorageKey::TokenPerOwnerInner {
+                account_id_hash: hash_account_id(&tmp_account_id),
+            }
+            .try_to_vec()
+            .unwrap(),
+        );
+        self.tokens_per_owner.insert(&tmp_account_id, &u);
+
+        let tokens_per_owner_entry_in_bytes = env::storage_usage() - initial_storage_usage;
+        let owner_id_extra_cost_in_bytes = (tmp_account_id.len() - self.owner_id.len()) as u64;
+
+        self.extra_storage_in_bytes_per_token =
+            tokens_per_owner_entry_in_bytes + owner_id_extra_cost_in_bytes;
+
+        self.tokens_per_owner.remove(&tmp_account_id);
     }
 }
--- contracts/src/mint.rs
+++ contracts/src/mint.rs
@@ -5,13 +5,41 @@ impl Contract {
     #[payable]
     pub fn nft_mint(
         &mut self,
-        token_id: Option<TokenId>,
+        token_id: TokenId,
         metadata: TokenMetadata,
-        perpetual_royalties: Option<HashMap<AccountId, u32>>,
         receiver_id: Option<ValidAccountId>,
     ) {
-        /*
-            FILL THIS IN
-        */
+        //measure the initial storage being used on the contract
+        let initial_storage_usage = env::storage_usage();
+
+        //set the token owner to the predecessor by default
+        let mut owner_id = env::predecessor_account_id();
+        //if we specify a receiver, we overwrite the owner to be that receiver ID
+        if let Some(receiver_id) = receiver_id {
+            owner_id = receiver_id.into();
+        }
+
+        //specify the token struct that contains the owner ID 
+        let token = Token {
+            owner_id,
+        };
+
+        //insert the token ID and token struct and make sure that the token doesn't exist
+        assert!(
+            self.tokens_by_id.insert(&token_id, &token).is_none(),
+            "Token already exists"
+        );
+
+        //insert the token ID and metadata
+        self.token_metadata_by_id.insert(&token_id, &metadata);
+        
+        //call the internal method for adding the token to the owner
+        self.internal_add_token_to_owner(&token.owner_id, &token_id);
+
+        let new_token_size_in_bytes = env::storage_usage() - initial_storage_usage;
+        let required_storage_in_bytes =
+            self.extra_storage_in_bytes_per_token + new_token_size_in_bytes;
+
+        refund_deposit(required_storage_in_bytes);
     }
 }
