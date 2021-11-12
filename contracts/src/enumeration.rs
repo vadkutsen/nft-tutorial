663c2b08e3e0e0e1ba65fdb7326a24d8120f2f6d
BenKurrek <bkurrek@uwaterloo.ca>
2021-11-12 11:14:52 -0500

    Followed same commenting and params as skeleton for non-minting related files

--- contracts/src/enumeration.rs
+++ contracts/src/enumeration.rs
@@ -3,12 +3,14 @@ use crate::*;
 #[near_bindgen]
 impl Contract {
 
+    //Query for nft tokens on the contract regardless of the owner using pagination
     pub fn nft_tokens(&self, from_index: Option<U128>, limit: Option<u64>) {
         /*
             FILL THIS IN
         */
     }
 
+    //get the total supply of NFTs for a given owner
     pub fn nft_supply_for_owner(
         &self,
         account_id: AccountId,
